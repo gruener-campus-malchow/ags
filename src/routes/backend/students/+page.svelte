d1c87a37688f6d74361e48a969d1301274ef53aa
Lorenzo von Flocken <vonflocken@duck.com>
2024-08-26 14:12:49 +0200

    fix example-students.csv

--- src/routes/backend/students/+page.svelte
+++ src/routes/backend/students/+page.svelte
@@ -7,9 +7,10 @@
 <h1 class="h1">AG-Verwaltung</h1>
 
 <div class="flex flex-wrap gap-4">
-    <form class="bg-gray-100 dark:bg-neutral-800 rounded-2xl p-4"
+    <form class="max-w-96 bg-gray-100 dark:bg-neutral-800 rounded-2xl p-4"
           action="?/upload_students" method="post" enctype="multipart/form-data" use:enhance>
-        <h2 class="h2 mt-0">CSV-Upload</h2>
+        <h2 class="h2 mt-0">CSV-Import</h2>
+        <p class="my-4">Hier kann eine komplette SuS-Liste im CSV-Format (<a class="text-green-600 underline" href="/example-students.csv">Beispiel-Datei</a>) hochgeladen werden.</p>
         <input type="file" name="csv" accept="text/csv" required>
         <div class="mt-4 flex justify-end">
             <button class="btn btn-primary">speichern</button>
@@ -19,8 +20,8 @@
     <div class="bg-gray-100 dark:bg-neutral-800 rounded-2xl py-4 max-h-40 overflow-auto">
         <table class="overflow-auto bg-inherit">
             <tr class="sticky bg-inherit -top-4">
-                <th>Vorname</th>
-                <th>Nachname</th>
+                <th class="px-4">Vorname</th>
+                <th class="px-4">Nachname</th>
             </tr>
             {#each data.students as student}
                 <tr class="border-t border-gray-300">
@@ -30,13 +31,4 @@
             {/each}
         </table>
     </div>
-
-    <ul class="bg-gray-100 dark:bg-neutral-800 rounded-2xl p-4 grid gap-x-4 grid-cols-[auto_auto]">
-        {#each data.students as student}
-            <li class="contents">
-                <span>{student.first_name}</span>
-                <span>{student.last_name}</span>
-            </li>
-        {/each}
-    </ul>
 </div>
